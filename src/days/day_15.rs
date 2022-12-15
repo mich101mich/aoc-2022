a2b43c8e7f4e880b9e073cc6427b6dfdb4ec3f9c
mhillmann <mhillmann@uos.de>
2022-12-15 15:37:37 +0100

    optimized day 15

--- src/days/day_15.rs
+++ src/days/day_15.rs
@@ -17,33 +17,16 @@ pub fn run() {
         .map(|(sx, sy, bx, by)| ((sx, sy), manhattan_i((sx, sy), (bx, by))))
         .to_vec();
 
-    let mut x = 0;
-    let mut y = 0;
-    while x <= 4000000 {
-        y = 0;
-        while y <= 4000000 {
-            let next_y = parsed
-                .iter()
-                .filter(|(s, d)| manhattan_i(*s, (x, y)) <= *d)
-                .map(|(s, d)| {
-                    let x_diff = (s.0 - x).abs();
-                    s.1 + (d - x_diff)
-                })
-                .max();
-            if let Some(ny) = next_y {
-                if ny == y {
-                    y += 1;
-                } else {
-                    y = ny;
-                }
-            } else {
-                pv!(x * 4000000 + y);
-                return;
-            }
-        }
-        x += 1;
-    }
-    unreachable!();
+    let area = 0..=4000000;
+
+    let (x, y) = parsed
+        .iter()
+        .flat_map(|(s, d)| manhattan_ring_iter(*s, d + 1))
+        .filter(|p| area.contains(&p.0) && area.contains(&p.1))
+        .find(|p| parsed.iter().all(|(s, d)| manhattan_i(*s, *p) > *d))
+        .unwrap();
+
+    pv!(x * 4000000 + y);
 }
 
 #[allow(unused)]
@@ -59,22 +42,35 @@ pub fn part_one() {
         .unwrap()
     });
 
-    let mut row = HashSet::new();
+    let mut ranges = vec![];
+    let mut beacons = HashSet::new();
 
     for (sx, sy, bx, by) in parsed {
+        if by == 2000000 {
+            beacons.insert(bx);
+        }
         let dist = manhattan_i((sx, sy), (bx, by));
         let y_diff = (sy - 2000000).abs();
         if dist <= y_diff {
             continue;
         }
-        let x_diff = (dist - y_diff).abs();
-        for x in (sx - x_diff)..=(sx + x_diff) {
-            if by == 2000000 && x == bx {
-                continue;
-            }
-            row.insert(x);
-        }
+        let x_diff = dist - y_diff;
+        ranges.push((sx - x_diff)..=(sx + x_diff));
     }
 
-    pv!(row.len());
+    ranges.sort_by_key(|r| *r.start());
+    let mut current = ranges[0].clone();
+    let mut sum = 0;
+    for r in &ranges[1..] {
+        if r.start() <= current.end() {
+            current = *current.start()..=*r.end().max(current.end());
+        } else {
+            sum += current.end() - current.start() + 1;
+            current = r.clone();
+        }
+    }
+    sum += current.end() - current.start() + 1;
+    sum -= beacons.len() as isize;
+
+    pv!(sum);
 }
