e94cd5285fdee3ae3a5dd931582695b494b3b7e9
mich101mich <mich101mich@gmail.com>
2022-12-17 10:01:41 +0100

    changed day 17 to use detect_increment_loop

--- src/days/day_17.rs
+++ src/days/day_17.rs
@@ -28,11 +28,50 @@ const SHAPES: [&[&str]; 5] = [
     ],
 ];
 
+type ShapeSize = (HashSet<(isize, isize)>, (isize, isize));
+
+fn run_step<'a>(
+    area: &mut Grid<bool>,
+    next_shape: &mut impl Iterator<Item = &'a ShapeSize>,
+    next_dir: &mut impl Iterator<Item = Dir>,
+    highest: &mut isize,
+) -> usize {
+    let (shape, (_, h)) = next_shape.next().unwrap();
+
+    let shape_points = |pos: (isize, isize)| shape.iter().map(move |p| (pos.0 + p.0, pos.1 + p.1));
+    let mut pos = (2, *highest - 3 - h);
+    let mut dirs_taken = 0;
+    loop {
+        let dir = next_dir.next().unwrap();
+        dirs_taken += 1;
+
+        if shape_points(pos)
+            .map(|p| p + dir)
+            .all(|p| area.in_bounds(p) && !area[p])
+        {
+            pos += dir;
+        }
+        let dir = Dir::Down;
+        if shape_points(pos)
+            .map(|p| p + dir)
+            .all(|p| area.in_bounds(p) && !area[p])
+        {
+            pos += dir;
+        } else {
+            break;
+        }
+    }
+    for p in shape_points(pos) {
+        area[p] = true;
+    }
+    *highest = pos.1.min(*highest);
+    dirs_taken
+}
+
 #[allow(unused)]
 pub fn run() {
     #[allow(unused_variables)]
     let input = include_str!("../input/17.txt");
-    // let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
 
     let next_shape = SHAPES
         .iter()
@@ -41,13 +80,13 @@ pub fn run() {
             let h = shape.len() as isize;
             let shape = shape.join("\n");
             let set = dotted_grid(&shape, '#')
-                .grid_iter_index()
-                .filter_map(|((x, y), t)| t.then_some((x as isize, y as isize)))
+                .pos_iter()
+                .map(|(x, y)| (x as isize, y as isize))
                 .to_set();
             (set, (w, h))
         })
         .to_vec();
-    let num_shapes = next_shape.len() as u64;
+    let num_shapes = next_shape.len();
     let mut next_shape = next_shape.iter().cycle();
     let mut shapes_taken = 0;
 
@@ -59,69 +98,26 @@ pub fn run() {
             c => panic!("Invalid direction: {}", c),
         })
         .to_vec();
-    let num_dirs = next_dir.len() as u64;
+    let num_dirs = next_dir.len();
     let mut next_dir = next_dir.iter().copied().cycle();
     let mut dirs_taken = 0;
 
-    let mut area = Grid::new_clone((7, 1000000), false);
+    let mut area = Grid::new_clone((7, 100000), false);
     let mut highest = area.h() as isize;
 
-    let mut seen_cycles = HashMap::new();
-    let mut height_offset = 0;
+    let height = detect_increment_loop(
+        1_000_000_000_000,
+        || {
+            shapes_taken += 1;
+            dirs_taken += run_step(&mut area, &mut next_shape, &mut next_dir, &mut highest);
 
-    let target = 1_000_000_000_000u64;
-    while shapes_taken < target {
-        let (shape, (w, h)) = next_shape.next().unwrap();
-        shapes_taken += 1;
+            let key = (shapes_taken % num_shapes, dirs_taken % num_dirs);
+            (key, highest as _)
+        },
+        (num_dirs * num_shapes) as _,
+    );
 
-        let shape_points =
-            |pos: (isize, isize)| shape.iter().map(move |p| (pos.0 + p.0, pos.1 + p.1));
-        let mut pos = (2, highest - 3 - h);
-        loop {
-            let dir = next_dir.next().unwrap();
-            dirs_taken = (dirs_taken + 1) % num_dirs;
-
-            if shape_points(pos)
-                .map(|p| p + dir)
-                .all(|p| area.in_bounds(p) && !area[p])
-            {
-                pos += dir;
-            }
-            let dir = Dir::Down;
-            if shape_points(pos)
-                .map(|p| p + dir)
-                .all(|p| area.in_bounds(p) && !area[p])
-            {
-                pos += dir;
-            } else {
-                break;
-            }
-        }
-        for p in shape_points(pos) {
-            area[p] = true;
-        }
-        highest = highest.min(pos.1);
-
-        if shapes_taken < num_dirs * num_shapes || height_offset > 0 {
-            continue;
-        }
-
-        let key = (shapes_taken % num_shapes, dirs_taken);
-        match seen_cycles.entry(key) {
-            Entry::Occupied(e) => {
-                let (cycle_start, start_height) = e.get();
-                let cycle_length = shapes_taken - cycle_start;
-                let cycle_gain = (area.h() as isize - highest) - start_height;
-                let cycles_left = (target - shapes_taken) / cycle_length;
-                shapes_taken += cycles_left * cycle_length;
-                height_offset = cycles_left as isize * cycle_gain;
-            }
-            Entry::Vacant(e) => {
-                e.insert((shapes_taken, area.h() as isize - highest));
-            }
-        }
-    }
-    let height = area.h() as isize - highest + height_offset;
+    let height = area.h() as i128 - height;
     pv!(height);
 }
 
@@ -137,8 +133,8 @@ pub fn part_one() {
             let h = shape.len() as isize;
             let shape = shape.join("\n");
             let set = dotted_grid(&shape, '#')
-                .grid_iter_index()
-                .filter_map(|((x, y), t)| t.then_some((x as isize, y as isize)))
+                .pos_iter()
+                .map(|(x, y)| (x as isize, y as isize))
                 .to_set();
             (set, (w, h))
         })
@@ -159,33 +155,7 @@ pub fn part_one() {
     let mut highest = area.h() as isize;
 
     for _ in 0..2022 {
-        let (shape, (w, h)) = next_shape.next().unwrap();
-        let shape_points =
-            |pos: (isize, isize)| shape.iter().map(move |p| (pos.0 + p.0, pos.1 + p.1));
-        let mut pos = (2, highest - 3 - h);
-        loop {
-            let dir = next_dir.next().unwrap();
-
-            if shape_points(pos)
-                .map(|p| p + dir)
-                .all(|p| area.in_bounds(p) && !area[p])
-            {
-                pos += dir;
-            }
-            let dir = Dir::Down;
-            if shape_points(pos)
-                .map(|p| p + dir)
-                .all(|p| area.in_bounds(p) && !area[p])
-            {
-                pos += dir;
-            } else {
-                break;
-            }
-        }
-        for p in shape_points(pos) {
-            area[p] = true;
-        }
-        highest = highest.min(pos.1);
+        run_step(&mut area, &mut next_shape, &mut next_dir, &mut highest);
     }
     let height = area.h() as isize - highest;
     pv!(height);
